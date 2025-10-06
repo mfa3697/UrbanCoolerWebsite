44c49ce53e4cd8bae9734df093687cf3b3b1788d
mfa3697 <mfa3697@students.fhv.at>
2025-10-06 18:31:48 +0200

    config.ts anpassung

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from "path"
+import path from "node:path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
@@ -6,6 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/landing-page/', // <- anpassen bei User-Page!
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
