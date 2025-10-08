b2c61e29f069cd1297ec68758dcf3e4c35c897b0
mfa3697 <mfa3697@students.fhv.at>
2025-10-08 17:51:27 +0200

    useMemo entfernt

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import type { ReactNode } from "react";
 import { motion } from "framer-motion";
 import {
